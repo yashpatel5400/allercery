0acf8409745a140fbfc4290482f46fa5e2aab07f
Yash Patel <ypatel@princeton.edu>
2017-11-11 02:43:54 -0500

    Started converting to Python (NON-functional)

---
+++ settings.py
@@ -0,0 +1,16 @@
+"""
+__name__ = settings.py
+__authors__ = HackPrinceton 2017 Best Team
+__description__ = Global settings file
+"""
+
+# --------------------- Overall Window ------------------------ #
+WINDOW_NAME = "result"
+
+# --------------------- Overall Window ------------------------ #
+SCALE = 4
+VERTICAL_DIVIDE = 1
+
+# thresholds for Canny edge detector
+THRESHOLD1 = 0
+THRESHOLD2 = 255
