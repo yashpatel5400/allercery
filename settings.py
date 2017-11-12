96e2fe7b6b6dfec2208de24ec68c2ab8febb50f6
Yash Patel <ypatel5400@gmail.com>
2017-11-11 22:13:43 -0500

    Better partitioning

--- settings.py
+++ settings.py
@@ -14,4 +14,8 @@ VERTICAL_DIVIDE = 1
 
 # thresholds for Canny edge detector
 THRESHOLD1 = 0
-THRESHOLD2 = 225
+THRESHOLD2 = 255
+
+# threshold of how large a "slice" of the image must be to be considered
+# a legal cut
+DIM_THRESHOLD = 10
